c79ab55317196fb1b7c644b71cb05d707d88018e
janak <janakbhatta898@gmail.com>
2026-08-02 01:29:03 +0545

    maywork

--- src/cgroups.c
+++ src/cgroups.c
@@ -24,7 +24,6 @@ int mkdir_cgroups(){
     return 0;
 
 }
-
 static int write_to_file(const char *path,const char *value){
     FILE *fp=fopen(path,"w");
     if(fp==NULL){
@@ -37,6 +36,21 @@ static int write_to_file(const char *path,const char *value){
     fclose(fp);
     return 0;
 }
+void max_cpu(int percent){
+    char value[64];
+    long period = 100000; //100ms
+    long quota = (period * percent) / 100;
+    snprintf(value, sizeof(value), "%ld %ld\n", quota, period);
+    write_to_file(cgroup_path"/cpu.max", value);
+}
+void max_swap(){
+    write_to_file(cgroup_path"/memory.swap.max","0\n");
+    return;
+}
+void max_memory(){
+    write_to_file(cgroup_path"/memory.max","16777216\n");
+    return;
+}
 void max_pids(){
     write_to_file(cgroup_path"/pids.max","100\n");
 
@@ -44,22 +58,20 @@ void max_pids(){
 
 
 }
-
 int attach_process_to_cgroup_procs(pid_t pid) {
    // 1. Format the integer PID into a string
    char pid_str[32];
-    snprintf(pid_str, sizeof(pid_str), "%d\n", pid); // or sprintf -> can cause memory overflow
+    snprintf(pid_str, sizeof(pid_str), "%d\n", pid); // or sprintf
     
     // linux store every data in character so need to convert integer pid to str which above line of code does
     return write_to_file("/sys/fs/cgroup/bandbox/cgroup.procs", pid_str);
 }
-
 int setup_cgroups(){
 
    
 //enable controllers in parent cgroup i.e cgroup.subtree_control so that the child process can excess the controllers of banbox
     if(write_to_file(cgroup_root"/cgroup.subtree_control","+cpu +memory +pids")==-1) return -1;
-    mkdir_cgroups();   
+     mkdir_cgroups();   
 
     max_pids();
     max_swap();
